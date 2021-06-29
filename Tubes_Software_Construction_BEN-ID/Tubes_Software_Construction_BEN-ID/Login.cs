40085432c4026492eeca30bbcfbd423a07151663
Fahmi Abdillah Al-Amien <63788366+fahmiab11@users.noreply.github.com>
2021-06-29 22:02:44 +0700

    LoginTerbaru

--- Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/Login.cs
+++ Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/Login.cs
@@ -19,7 +19,7 @@ namespace Tubes_Software_Construction_BEN_ID
 
         private void Login_Load(object sender, EventArgs e)
         {
-
+            Tpassword.UseSystemPasswordChar = true;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -32,15 +32,67 @@ namespace Tubes_Software_Construction_BEN_ID
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Tlogin_Click(object sender, EventArgs e)
         {
-            new Profile().Show();
+            string username;
+            string password;
+            username = "fahmiab11";
+            password = "password";
+
+            if (Tusername.Text == "" || Tpassword.Text == "")
+            {
+                MessageBox.Show("Mohon isikan username dan password terlebih dahulu!");
+            }
+            else
+            {
+                if (Tusername.Text == username && Tpassword.Text == password)
+                {
+                    MessageBox.Show("Login Berhasil!");
+                    Tuser.Visible = false;
+                    Tpass.Visible = false;
+                    Tusername.Visible = false;
+                    Tpassword.Visible = false;
+                    new Menu().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username dan password yang Anda masukkan salah!");
+                }
+            }
+        }
+
+        private void Tsignup_Click(object sender, EventArgs e)
+        {
+            new SignUp().Show();
             this.Hide();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Tusername_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Tpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tlihat_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Tlihat.Checked)
+            {
+                Tpassword.UseSystemPasswordChar = false;
+            }
+            else if (Tlihat.Checked)
+            {
+                Tpassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void Tkeluar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
