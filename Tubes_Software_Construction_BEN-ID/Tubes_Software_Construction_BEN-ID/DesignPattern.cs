0eb65c99f80acd75a6d08764eeecc656f2e0b329
M. Habibullah Alfatah Bakara <alfatah.bakara02@gmail.com>
2021-06-29 22:37:04 +0700

    Design pattern (Isi BBM)

---
+++ Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/DesignPattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_Software_Construction_BEN_ID
+{
+    class DesignPattern
+    {
+        // Komponen pada enum status.
+        public enum Status
+        {
+            Empty, Pengisian, Pembayaran
+        }
+
+        private static readonly Lazy<SingletonPattern> _singleton = new Lazy<SingletonPattern>(() => new SingletonPattern());
+
+        public static SingletonPattern GetInstance() => _singleton.Value;
+
+        // Method homepage.
+        private Status homepage;
+
+        // Kondisi dimana pada tampilan masih kosong.
+        private SingletonPattern()
+        {
+            Console.WriteLine("login");
+            homepage = Status.Empty;
+        }
+
+        // Kondisi dimana pada tampilan isi bbm status sebagai pengisian pembelian.
+        public void isiBBM()
+        {
+            if (!IsEmpty) return;
+            Console.WriteLine("isi pengisian");
+            homepage = Status.Pengisian;
+        }
+
+
+        // Kondisi dimana pada tampilan pembayaran status sebagai pembayaran dalam pengisian BBM.
+        public void Pembayaran()
+        {
+            if (IsPembayaran || IsEmpty) return;
+            Console.WriteLine("bayar");
+            homepage = Status.Pembayaran;
+        }
+
+        private bool IsEmpty => (homepage == Status.Empty);
+
+        private bool IsPembayaran => (homepage == Status.Pembayaran);
+    }
+}
--- Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/isiBBM.Designer.cs
+++ Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/isiBBM.Designer.cs
@@ -30,11 +30,17 @@ namespace Tubes_Software_Construction_BEN_ID
         private void InitializeComponent()
         {
             this.LinkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.Button4 = new System.Windows.Forms.Button();
-            this.Button3 = new System.Windows.Forms.Button();
-            this.Button2 = new System.Windows.Forms.Button();
-            this.Button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.TxtTotal = new System.Windows.Forms.Label();
+            this.TxtHarga = new System.Windows.Forms.Label();
+            this.CmbLiter = new System.Windows.Forms.ComboBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbBbm = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LinkLabel1
@@ -45,50 +51,10 @@ namespace Tubes_Software_Construction_BEN_ID
             this.LinkLabel1.LinkColor = System.Drawing.Color.Black;
             this.LinkLabel1.Location = new System.Drawing.Point(118, 94);
             this.LinkLabel1.Name = "LinkLabel1";
-            this.LinkLabel1.Size = new System.Drawing.Size(150, 39);
+            this.LinkLabel1.Size = new System.Drawing.Size(142, 38);
             this.LinkLabel1.TabIndex = 9;
             this.LinkLabel1.TabStop = true;
             this.LinkLabel1.Text = "ISI BBM";
-            // 
-            // Button4
-            // 
-            this.Button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Button4.Location = new System.Drawing.Point(215, 359);
-            this.Button4.Name = "Button4";
-            this.Button4.Size = new System.Drawing.Size(122, 78);
-            this.Button4.TabIndex = 8;
-            this.Button4.Text = "PERTAMAX TURBO";
-            this.Button4.UseVisualStyleBackColor = true;
-            // 
-            // Button3
-            // 
-            this.Button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Button3.Location = new System.Drawing.Point(40, 359);
-            this.Button3.Name = "Button3";
-            this.Button3.Size = new System.Drawing.Size(122, 78);
-            this.Button3.TabIndex = 7;
-            this.Button3.Text = "PERTAMAX";
-            this.Button3.UseVisualStyleBackColor = true;
-            // 
-            // Button2
-            // 
-            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Button2.Location = new System.Drawing.Point(215, 223);
-            this.Button2.Name = "Button2";
-            this.Button2.Size = new System.Drawing.Size(122, 78);
-            this.Button2.TabIndex = 6;
-            this.Button2.Text = "SOLAR";
-            this.Button2.UseVisualStyleBackColor = true;
-            // 
-            // Button1
-            // 
-            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Button1.Location = new System.Drawing.Point(40, 223);
-            this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(122, 78);
-            this.Button1.TabIndex = 5;
-            this.Button1.Text = "PERTALITE";
-            this.Button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -100,21 +66,139 @@ namespace Tubes_Software_Construction_BEN_ID
             this.label1.TabIndex = 10;
             this.label1.Text = "Silahkan Memilih Bensin";
             // 
+            // TxtTotal
+            // 
+            this.TxtTotal.AutoSize = true;
+            this.TxtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTotal.Location = new System.Drawing.Point(73, 451);
+            this.TxtTotal.Name = "TxtTotal";
+            this.TxtTotal.Size = new System.Drawing.Size(16, 20);
+            this.TxtTotal.TabIndex = 23;
+            this.TxtTotal.Text = "-";
+            // 
+            // TxtHarga
+            // 
+            this.TxtHarga.AutoSize = true;
+            this.TxtHarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHarga.Location = new System.Drawing.Point(73, 302);
+            this.TxtHarga.Name = "TxtHarga";
+            this.TxtHarga.Size = new System.Drawing.Size(16, 20);
+            this.TxtHarga.TabIndex = 22;
+            this.TxtHarga.Text = "-";
+            // 
+            // CmbLiter
+            // 
+            this.CmbLiter.FormattingEnabled = true;
+            this.CmbLiter.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.CmbLiter.Location = new System.Drawing.Point(73, 369);
+            this.CmbLiter.Name = "CmbLiter";
+            this.CmbLiter.Size = new System.Drawing.Size(101, 24);
+            this.CmbLiter.TabIndex = 21;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.BackColor = System.Drawing.Color.Transparent;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.Location = new System.Drawing.Point(70, 349);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(98, 17);
+            this.Label4.TabIndex = 20;
+            this.Label4.Text = "Berapa Liter";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.BackColor = System.Drawing.Color.Transparent;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(70, 434);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(94, 17);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "Total Harga";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.BackColor = System.Drawing.Color.Transparent;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(73, 285);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(52, 17);
+            this.Label2.TabIndex = 18;
+            this.Label2.Text = "Harga";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Memilih BBM";
+            // 
+            // cmbBbm
+            // 
+            this.cmbBbm.FormattingEnabled = true;
+            this.cmbBbm.Items.AddRange(new object[] {
+            "PERTAMAX",
+            "SOLAR",
+            "PERTAMAX TURBO",
+            "PERTALITE"});
+            this.cmbBbm.Location = new System.Drawing.Point(76, 229);
+            this.cmbBbm.Name = "cmbBbm";
+            this.cmbBbm.Size = new System.Drawing.Size(225, 24);
+            this.cmbBbm.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(102, 579);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 17);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Melanjutkan Pembayaran";
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(136, 548);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(105, 28);
+            this.Button1.TabIndex = 24;
+            this.Button1.Text = "LANJUT";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
             // isiBBM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Tubes_Software_Construction_BEN_ID.Properties.Resources.Search2;
             this.ClientSize = new System.Drawing.Size(370, 680);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TxtTotal);
+            this.Controls.Add(this.TxtHarga);
+            this.Controls.Add(this.CmbLiter);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbBbm);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.LinkLabel1);
-            this.Controls.Add(this.Button4);
-            this.Controls.Add(this.Button3);
-            this.Controls.Add(this.Button2);
-            this.Controls.Add(this.Button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "isiBBM";
             this.Text = "isiBBM";
+            this.Load += new System.EventHandler(this.isiBBM_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,10 +207,16 @@ namespace Tubes_Software_Construction_BEN_ID
         #endregion
 
         internal System.Windows.Forms.LinkLabel LinkLabel1;
-        internal System.Windows.Forms.Button Button4;
-        internal System.Windows.Forms.Button Button3;
-        internal System.Windows.Forms.Button Button2;
-        internal System.Windows.Forms.Button Button1;
         private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label TxtTotal;
+        internal System.Windows.Forms.Label TxtHarga;
+        internal System.Windows.Forms.ComboBox CmbLiter;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label label5;
+        internal System.Windows.Forms.ComboBox cmbBbm;
+        internal System.Windows.Forms.Label label6;
+        internal System.Windows.Forms.Button Button1;
     }
 }
