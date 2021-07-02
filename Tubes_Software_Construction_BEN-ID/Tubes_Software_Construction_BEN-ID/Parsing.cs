2692639c6df575380de85c750a98a9073cb35aea
Noval Fauzan <nouval.bendang@gmail.com>
2021-07-02 21:01:24 +0700

    parsingggg

--- Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/Parsing.cs
+++ Tubes_Software_Construction_BEN-ID/Tubes_Software_Construction_BEN-ID/Parsing.cs
@@ -3,55 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using Newtonsoft.Json;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+using System.Xml;
+using Tubes_Software_Construction_BEN_ID;
 
 namespace Tubes_Software_Construction_BEN_ID
 {
-    class Parsing
+    class parsing
     {
-
-        public string NamaPenggguna { get; set; }
-        public string Nomortelepon { get; set; }
+        public string NamaPengguna { get; set; }
         public string Email { get; set; }
+        public string NomorTelepon { get; set; }
     }
-    public sealed partial class MainPage : Page
+}
+public sealed partial class MainPage : Page
+{
+    private object email;
+
+    public MainPage()
     {
-        public object JsonConvert { get; private set; }
-        public object email { get; private set; }
-        public object namapengg { get; private set; }
-        public object notelp { get; private set; }
-
-        public MainPage()
-        {
-            NewMethod();
-        }
-
-        private void NewMethod()
-        {
-            NewMethod1();
-        }
-
-        private void NewMethod1()
-        {
-            this.InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Person person = new Person();
-            person.NamaPengguna = namapengg.Text;
-            person.Nomortelepon = notelp.Text;
-            person.Email = email.Text;
-            string json = JsonConvert.SerializeObject(
-                person, Formatting.Indented);
-        }
+        this.InitializeComponent();
     }
 
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public object notelp { get; private set; }
+    public object JsonConvert { get; private set; }
+    public object namapengg { get; private set; }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Person person = new Person();
+        person.NamaPengguna = namapengg.Text;
+        person.Email = email.Text;
+        person.NomorTelepon = notelp.Text;
+        string json = JsonConvert.SerializeObject(person, Formatting.Indented);
+    }
 }
